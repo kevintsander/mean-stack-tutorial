53bc76346c8987d5377e8e255bd63a705c4ca0d5
Kevin Sander <kevintsander@gmail.com>
2022-10-21 16:21:56 -0400

    added ability to save posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-course';
+  storedPosts: { title: string, content: string }[] = [];
+
+  onPostAdded(post: any) {
+    this.storedPosts.push(post);
+    console.dir(this.storedPosts)
+  }
 }
