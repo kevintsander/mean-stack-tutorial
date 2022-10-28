588e72094853e0e7f260c1fa54592201832cb09c
Kevin Sander <kevintsander@gmail.com>
2022-10-28 17:26:45 -0400

    added http post request when adding message posts

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-      "Acces-Control-Allow-Headers",
+      "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.setHeader(
@@ -15,7 +16,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/posts',(req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: false })); // not actually used, just shown as part of training to indicate other types of data besides json can be parsed
+
+app.post('/api/posts', (req, res, next) => {
+  const posts = req.body;
+  console.log(posts);
+  res.status(201).json({
+    message: "Post added successfully!"
+  });
+});
+
+app.get('/api/posts',(req, res, next) => {
   const posts = [
     { id: '1', title: 'First server-side post', content: 'This is coming from the server'},
     { id: '2', title: 'Second server-side post', content: 'This second set of data is coming from the server'}
