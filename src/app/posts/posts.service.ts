177dbd1b32239e898f57e70839d751ff5ff07098
Kevin Sander <kevintsander@gmail.com>
2022-10-31 17:24:34 -0400

    allow newly added posts to be deleted without reloading page

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,8 +36,9 @@ export class PostsService {
   addPosts(title: string, content: string) {
     const post: Post = { id: '', title: title, content: content };
     this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
-      .subscribe((responseData) => {
-        console.log(responseData.message);
+      .subscribe((responseData: any) => {
+        const id = responseData.postId;
+        post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
