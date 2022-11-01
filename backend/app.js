40e0765ac44451edd8a92118e460a4e2751867af
Kevin Sander <kevintsander@gmail.com>
2022-11-01 17:19:38 -0400

    separated backend routes into separate file

--- backend/app.js
+++ backend/app.js
@@ -1,88 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const config = require('./config.json');
+const config = require("./config.json");
 
-const Post = require('./models/post'); // capital starting character convention - allows you to define object based on 'blueprint'
+const postRoutes = require("./routes/posts");
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${config['db']['user']}:${config['db']['password']}@cluster0.g7oxtkv.mongodb.net/${config['db']['db-name']}?retryWrites=true&w=majority`)
+mongoose
+  .connect(
+    `mongodb+srv://${config["db"]["user"]}:${config["db"]["password"]}@cluster0.g7oxtkv.mongodb.net/${config["db"]["db-name"]}?retryWrites=true&w=majority`
+  )
   .then(() => {
-    console.log('Connected to database!');
+    console.log("Connected to database!");
   })
   .catch(() => {
-    console.log('Connection failed!');
+    console.log("Connection failed!");
   });
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: false })); // not actually used, just shown as part of training to indicate other types of data besides json can be parsed
+app.use(bodyParser.urlencoded({ extended: false })); // not actually used, just shown as part of training to indicate other types of data besides json can be parsed
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added successfully!",
-      postId: createdPost._id
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: "Update successful!"});
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: documents
-      });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  console.log('test');
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found!' })
-    }
-  })
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Post deleted!' });
-  });
-});
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
