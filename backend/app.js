ffb4ed03d09f359478de97cdd434fa9c0100d18d
Kevin Sander <kevintsander@gmail.com>
2022-11-01 17:04:21 -0400

    edited posts now loaded from backend

--- backend/app.js
+++ backend/app.js
@@ -50,12 +50,12 @@ app.put("/api/posts/:id", (req, res, next) => {
     _id: req.body.id,
     title: req.body.title,
     content: req.body.content
-  })
+  });
   Post.updateOne({_id: req.params.id}, post).then(result => {
     console.log(result);
     res.status(200).json({message: "Update successful!"});
-  })
-})
+  });
+});
 
 app.get('/api/posts', (req, res, next) => {
   Post.find()
@@ -67,11 +67,22 @@ app.get('/api/posts', (req, res, next) => {
     });
 });
 
+app.get('/api/posts/:id', (req, res, next) => {
+  console.log('test');
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found!' })
+    }
+  })
+});
+
 app.delete('/api/posts/:id', (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
-    res.status(200).json({ message: 'Post deleted!'});
+    res.status(200).json({ message: 'Post deleted!' });
   });
-})
+});
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -34,7 +34,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return { ...this.posts.find(p => p.id === id) } as Post;
+    return this.http.get<{ _id: string, title: string, content: string }>(`http://localhost:3000/api/posts/${id}`);
   }
 
   addPost(title: string, content: string) {
@@ -52,7 +52,13 @@ export class PostsService {
     const post: Post = { id: id, title: title, content: content }
     this.http
       .put(`http://localhost:3000/api/posts/${id}`, post)
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   deletePost(postId: string) {
