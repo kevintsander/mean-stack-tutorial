ff87a021f1f4e3bb8119bd085e4e98910347c8d5
Kevin Sander <kevintsander@gmail.com>
2022-11-02 16:39:04 -0400

    added mime image validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,7 +28,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, SelectMultipleControlValueAccessor } from '@angular/forms'
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms'
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -13,12 +14,27 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit {
   post: Post = {} as Post;
   isLoading: boolean = false;
+  form: FormGroup;
+  imagePreview: string;
   private mode: string = 'create';
   private postId: string | null;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'title': new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      'content': new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      'image': new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
@@ -29,6 +45,10 @@ export class PostCreateComponent implements OnInit {
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
           this.post = { id: postData._id, title: postData.title, content: postData.content };
+          this.form.setValue({
+            'title': this.post.title,
+            'content': this.post.content
+          })
         });
       } else {
         this.mode = 'create';
@@ -37,16 +57,27 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image')?.updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    if (file) reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postsService.addPost(form.value.title, form.value.content)
+      this.postsService.addPost(this.form.value.title, this.form.value.content)
     } else {
-      if (this.postId) this.postsService.updatePost(this.postId, form.value.title, form.value.content)
+      if (this.postId) this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content)
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
