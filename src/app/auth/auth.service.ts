a8498212cc85a2f4d39a0d6c662cc2c14e67c4d1
Kevin Sander <kevintsander@gmail.com>
2022-11-05 12:23:06 -0400

    update ui when token expires

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { AuthData } from "./auth-data.model";
 export class AuthService {
 
   private token: string | null;
+  private tokenTimer: NodeJS.Timer;
   private authStatusListener = new Subject<boolean>();
   private isAuthenticated = false;
 
@@ -37,10 +38,14 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post<{ token: string }>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{ token: string, expiresIn: number }>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         this.token = response.token;
         if (this.token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000)
           this.isAuthenticated = true;
           this.authStatusListener.next(this.isAuthenticated); // inform observers that we are authenticated
           this.router.navigate(['/']);
@@ -52,6 +57,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 
