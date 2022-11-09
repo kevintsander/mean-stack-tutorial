b2c45ebf05d22867063d5ab8953c108989f92077
Kevin Sander <kevintsander@gmail.com>
2022-11-09 10:21:46 -0500

    split routes into separate files to allow lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { ErrorInterceptor } from './error/error-interceptor';
 import { ErrorComponent } from './error/error.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/posts.module';
-import { AuthModule } from './auth/auth-module';
 
 @NgModule({
   declarations: [
@@ -30,7 +29,6 @@ import { AuthModule } from './auth/auth-module';
     HttpClientModule,
     AngularMaterialModule,
     PostsModule,
-    AuthModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
